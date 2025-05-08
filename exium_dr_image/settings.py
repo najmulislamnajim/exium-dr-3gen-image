0645c39008b390aed7dfe06f4f71ca510982e973
najmulislamnajim <ninbd17@gmail.com>
2025-05-08 11:55:02 +0600

    remove browser reload

--- exium_dr_image/settings.py
+++ exium_dr_image/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    "django_browser_reload",
+    # "django_browser_reload", # Development Only
 ]
 
 MIDDLEWARE = [
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    # "django_browser_reload.middleware.BrowserReloadMiddleware", # Development Only
 ]
 
 ROOT_URLCONF = 'exium_dr_image.urls'
