70e6566e0db4bcb88f8ca27d5e0665a13510961e
najmulislamnajim <ninbd17@gmail.com>
2025-05-08 11:58:07 +0600

    add urls

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/logout/', views.user_logout, name='logout'),
     path('upload-preview', views.upload_preview, name='upload_preview'),
-    path('upload/<int:instance_id>', views.upload, name='upload'),
+    path('upload/<int:instance_id>/', views.upload, name='upload'),
     path('territories/', views.territory_list_page, name='territory_list'),
     path('territory/<str:territory_code>/', views.territory_detail, name='territory_detail'),
     path('download/all', views.download, name='download_all'),
     path('export-excel', views.export_excel, name='export_excel'),
+    path('doctor/<int:instance_id>', views.doctor_view, name='doctor_view'),
+    path('delete-doctor/<int:instance_id>', views.delete_doctor, name='delete_doctor'),
 ]
